0752043a7a9f7ab7de51259e756a21533dbf0705
cmsjulio <cms.julio1@gmail.com>
2023-08-27 01:37:07 -0300

    feat(Role): :sparkles: refatora Role de enum para classe

--- src/main/java/com/fotografocomvc/domain/model/Role.java
+++ src/main/java/com/fotografocomvc/domain/model/Role.java
@@ -1,6 +1,28 @@
-package com.fotografocomvc.domain.model.enums;
+package com.fotografocomvc.domain.model;
 
-public enum Role {
-    USER,
-    ADMIN
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="T_ROLE")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "ID_ROLE", nullable = false, unique = true)
+    private Long id;
+
+    @Column(name = "NM_ROLE", nullable = false, unique = true)
+    private String name;
+
 }

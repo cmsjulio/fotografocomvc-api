5268068e50a8ca3ead33171421a154d38d4ef6cc
cmsjulio <cms.julio1@gmail.com>
2023-08-27 01:07:30 -0300

    feat(): :sparkles: implementação inical de securityconfig

---
+++ src/main/java/com/fotografocomvc/api/security/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.fotografocomvc.api.security;
+
+import com.fotografocomvc.domain.model.BaseUser;
+import com.fotografocomvc.domain.model.enums.Role;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+
+    private static final String[] PUBLIC_ACCESS = {
+            "/photographer/**",
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, PUBLIC_ACCESS).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users(){
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+
+    }
+}
---
+++ src/main/java/com/fotografocomvc/domain/model/enums/Role.java
@@ -0,0 +1,6 @@
+package com.fotografocomvc.domain.model.enums;
+
+public enum Role {
+    USER,
+    ADMIN
+}

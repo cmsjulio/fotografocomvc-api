e2cc63c816011c90b5ff7139bb9534b02f05f6fc
cmsjulio <cms.julio1@gmail.com>
2023-08-27 15:39:55 -0300

    refactor(RoleRepository): :hammer: altera nome da variável de email para roleName

--- src/main/java/com/fotografocomvc/domain/repository/RoleRepository.java
+++ src/main/java/com/fotografocomvc/domain/repository/RoleRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-    Optional<Role> findByName(String email);
+    Optional<Role> findByName(String roleName);
 }

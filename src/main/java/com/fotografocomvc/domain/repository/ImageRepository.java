6be05220ef58ef0e63e206cf76f6338b64d2495a
cmsjulio <cms.julio1@gmail.com>
2023-08-29 01:32:48 -0300

    feat(ImageRepository): :sparkles: criar camada repository de image

---
+++ src/main/java/com/fotografocomvc/domain/repository/ImageRepository.java
@@ -0,0 +1,10 @@
+package com.fotografocomvc.domain.repository;
+
+import com.fotografocomvc.domain.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+    Optional<Image> findByName(String name);
+}

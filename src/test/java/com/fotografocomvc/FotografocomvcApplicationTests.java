d831f4ba5df34cb85dff5db5d6a85da91ae317fd
cmsjulio <cms.julio1@gmail.com>
2023-08-29 01:29:55 -0300

    :poop: gambiarras no teste para debugar código

--- src/test/java/com/fotografocomvc/FotografocomvcApplicationTests.java
+++ src/test/java/com/fotografocomvc/FotografocomvcApplicationTests.java
@@ -1,14 +1,26 @@
 package com.fotografocomvc;
 
 import com.fotografocomvc.domain.model.BaseUser;
+import com.fotografocomvc.domain.model.Image;
 import com.fotografocomvc.domain.model.Photographer;
 import com.fotografocomvc.domain.repository.BaseUserRepository;
+import com.fotografocomvc.domain.repository.ImageRepository;
 import com.fotografocomvc.domain.repository.PhotographerRepository;
+import com.fotografocomvc.domain.util.ImageUtility;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
 import java.util.Optional;
 
 @SpringBootTest
@@ -20,24 +32,102 @@ class FotografocomvcApplicationTests {
 	@Autowired
 	private BaseUserRepository baseUserRepository;
 
+	@Autowired
+	private ImageRepository imageRepository;
+
 	@Test
-	void contextLoads() {
-		BaseUser baseUser = BaseUser.builder()
-				.id(1L)
-				.username("email")
-				.password("password")
-				.build();
+	void contextLoads() throws IOException {
 
-		baseUserRepository.save(baseUser);
+		File resultImageFile = new File("/tmp/fodase");
 
-		Photographer photographer = Photographer.builder()
-				.name("julio")
-				.id(1L)
-				.baseUser(baseUser)
-				.build();
+		File origFile = new File("/tmp/source.png");
+		ImageIcon icon = new ImageIcon(origFile.getPath());
 
-		photographer = photographerRepository.save(photographer);
-		System.out.println(photographer);
+		// create BufferedImage object of same width and height as of original image
+		BufferedImage bufferedImage = new BufferedImage(icon.getIconWidth(),
+				icon.getIconHeight(), BufferedImage.TYPE_INT_RGB);
+
+		// create graphics object and add original image to it
+		Graphics graphics = bufferedImage.getGraphics();
+		graphics.drawImage(icon.getImage(), 0, 0, null);
+
+		File watermarkImageFile = new File("/tmp/watermark.png");
+		ImageIcon icon2 = new ImageIcon(watermarkImageFile.getPath());
+
+		// create BufferedImage object of same width and height as of original image
+		BufferedImage bufferedImage2 = new BufferedImage(icon2.getIconWidth(),
+				icon2.getIconHeight(), BufferedImage.TYPE_INT_RGB);
+
+		// create graphics object and add original image to it
+		Graphics graphics2 = bufferedImage2.getGraphics();
+		graphics2.drawImage(icon2.getImage(), 0, 0, null);
+
+		// initializes necessary graphic properties
+		Graphics2D g2d = (Graphics2D) bufferedImage.getGraphics();
+		AlphaComposite alphaChannel = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.3f);
+		g2d.setComposite(alphaChannel);
+
+		// calculates the coordinate where the image is painted
+		int topLeftX = (bufferedImage.getWidth() - bufferedImage2.getWidth()) / 2;
+		int topLeftY = (bufferedImage.getHeight() - bufferedImage2.getHeight()) / 2;
+
+		// paints the image watermark
+		g2d.drawImage(bufferedImage2, topLeftX, topLeftY, null);
+
+		ImageIO.write(bufferedImage, "png", resultImageFile);
+		g2d.dispose();
+
+		Files.readAllBytes(resultImageFile.toPath());
+
+
+		// FUNCIONA ABAIXO:
+//		File origFile = new File("/tmp/teste.jpeg");
+//		ImageIcon icon = new ImageIcon(origFile.getPath());
+//
+//		// create BufferedImage object of same width and height as of original image
+//		BufferedImage bufferedImage = new BufferedImage(icon.getIconWidth(),
+//				icon.getIconHeight(), BufferedImage.TYPE_INT_RGB);
+//
+//		// create graphics object and add original image to it
+//		Graphics graphics = bufferedImage.getGraphics();
+//		graphics.drawImage(icon.getImage(), 0, 0, null);
+//
+//		// set font for the watermark text
+//		graphics.setFont(new Font("Arial", Font.BOLD, 30));
+//
+//		//unicode characters for (c) is \u00a9
+//		String watermark = "\u00a9 JavaXp.com";
+//
+//		// add the watermark text
+//		graphics.drawString(watermark, 0, icon.getIconHeight() / 2);
+//		graphics.dispose();
+//
+//		File newFile = new File("/tmp/WatermarkedImage.jpg");
+//		try {
+//			ImageIO.write(bufferedImage, "jpg", newFile);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//
+//		System.out.println(newFile.getPath() + " created successfully!");
+
+
+//		BaseUser baseUser = BaseUser.builder()
+//				.id(1L)
+//				.username("email")
+//				.password("password")
+//				.build();
+//
+//		baseUserRepository.save(baseUser);
+//
+//		Photographer photographer = Photographer.builder()
+//				.name("julio")
+//				.id(1L)
+//				.baseUser(baseUser)
+//				.build();
+//
+//		photographer = photographerRepository.save(photographer);
+//		System.out.println(photographer);
 //
 //		Optional<Photographer> photographer2 =		photographerRepository.findById(1L);
 //
@@ -47,6 +137,10 @@ class FotografocomvcApplicationTests {
 //		System.out.println(photographer2);
 //		System.out.println(photographer3);
 
+	}
+
+	@Test
+	void writeFile(){
 
 	}
 

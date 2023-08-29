a216ac06c27a578459609161b30a151281d0c094
cmsjulio <cms.julio1@gmail.com>
2023-08-29 01:29:17 -0300

    chore(SecurityConstraints): :wrench: altera tempo de vida útil do token para 30 minutos

--- src/main/java/com/fotografocomvc/api/security/SecurityConstants.java
+++ src/main/java/com/fotografocomvc/api/security/SecurityConstants.java
@@ -1,6 +1,6 @@
 package com.fotografocomvc.api.security;
 
 public class SecurityConstants {
-    public static final Long JWT_EXPIRATION = 70000L;
+    public static final Long JWT_EXPIRATION = 1000L * 60 * 30;
     public static final String JWT_SECRET = "secret";
 }

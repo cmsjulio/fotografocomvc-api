0dbf45510eb3edd28457ba5c22e2a5b8f7ca4e66
cmsjulio <cms.julio1@gmail.com>
2023-08-29 01:32:11 -0300

    feat(Image): :sparkles: cria entidade image

---
+++ src/main/java/com/fotografocomvc/domain/model/Image.java
@@ -0,0 +1,33 @@
+package com.fotografocomvc.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="T_IMAGE")
+public class Image {
+
+    @Id
+    @Column(name = "ID_IMAGE")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "NM_NAME")
+    private String name;
+
+    @Column(name = "TP_FORMAT_TYPE")
+    private String formatType;
+
+    @Column(name = "BT_ORIGINAL_IMAGE", nullable = true, length = 100000)
+    private byte[] originalImage;
+
+    @Column(name = "BT_WATERMARKED_IMAGE", nullable = true, length = 100000)
+    private byte[] watermarkedImage;
+}

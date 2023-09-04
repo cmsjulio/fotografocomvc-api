624362c11e5002f94b15d5763f7f6ad9110bc60c
cmsjulio <cms.julio1@gmail.com>
2023-09-03 23:06:56 -0300

    chore(SecurityConstants): :wrench: adiciona tempo de expiração do refreshToken

--- src/main/java/com/fotografocomvc/api/security/SecurityConstants.java
+++ src/main/java/com/fotografocomvc/api/security/SecurityConstants.java
@@ -1,6 +1,8 @@
 package com.fotografocomvc.api.security;
 
 public class SecurityConstants {
-    public static final Long JWT_EXPIRATION = 1000L * 60 * 30;
+    public static final Long JWT_EXPIRATION = 1000L * 60 * 10;
     public static final String JWT_SECRET = "secret";
+
+    public static final Long REFRESH_TOKEN_EXPIRATION = 1000L * 10;
 }

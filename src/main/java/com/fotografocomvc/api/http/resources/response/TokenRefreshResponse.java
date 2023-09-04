6c587de63d2e3a0567f1cccf3a527819edd8f421
cmsjulio <cms.julio1@gmail.com>
2023-09-03 23:23:57 -0300

    feat(TokenRefreshResponse): :sparkles: cria TokenRefreshResponse

---
+++ src/main/java/com/fotografocomvc/api/http/resources/response/TokenRefreshResponse.java
@@ -0,0 +1,22 @@
+package com.fotografocomvc.api.http.resources.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenRefreshResponse {
+  private String accessToken;
+  private String refreshToken;
+  private String tokenType = "Bearer";
+
+  public TokenRefreshResponse(String accessToken, String refreshToken) {
+    this.accessToken = accessToken;
+    this.refreshToken = refreshToken;
+  }
+
+}

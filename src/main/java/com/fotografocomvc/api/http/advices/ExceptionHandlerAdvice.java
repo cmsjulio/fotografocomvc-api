23d2355d4ea785219c25136cbf4052fd7706caac
cmsjulio <cms.julio1@gmail.com>
2023-09-03 23:01:12 -0300

    feat(ExceptionHandlerAdvice): :sparkles: implementa exceptionHander para as exceções já mapeadas

--- src/main/java/com/fotografocomvc/api/http/advices/ExceptionHandlerAdvice.java
+++ src/main/java/com/fotografocomvc/api/http/advices/ExceptionHandlerAdvice.java
@@ -1,36 +1,76 @@
 package com.fotografocomvc.api.http.advices;
 
+import com.fotografocomvc.domain.exception.BusinessException;
 import com.fotografocomvc.domain.exception.NotFoundException;
+import com.fotografocomvc.domain.exception.TokenRefreshException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class ExceptionHandlerAdvice extends ResponseEntityExceptionHandler {
 
+    @ExceptionHandler(value = {BusinessException.class})
+    public ErrorMessage handleBusinessException(RuntimeException ex, WebRequest request) {
+        return new ErrorMessage(
+                HttpStatus.CONFLICT.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+    }
+
+
+    @ExceptionHandler(value = AuthenticationCredentialsNotFoundException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorMessage handleAuthenticationCredentialsNotFoundException(AuthenticationCredentialsNotFoundException ex, WebRequest request) {
+        return new ErrorMessage(
+                HttpStatus.UNAUTHORIZED.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+    }
+
+    @ExceptionHandler(value = AuthenticationException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorMessage handleAuthenticationNotFoundException(AuthenticationException ex, WebRequest request) {
+        return new ErrorMessage(
+                HttpStatus.UNAUTHORIZED.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+    }
+
+    @ExceptionHandler(value = TokenRefreshException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ErrorMessage handleTokenRefreshException(TokenRefreshException ex, WebRequest request) {
+        return new ErrorMessage(
+                HttpStatus.FORBIDDEN.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+    }
+
     @ExceptionHandler(value = {NotFoundException.class})
-    public ResponseEntity<Object> handleNotFoundException(RuntimeException ex, WebRequest request) {
-        NotFoundException exception = (NotFoundException) ex;
-        Map<String, Object> responseBody = exception.getError();
-
-        if (responseBody == null) {
-            responseBody = new HashMap<>();
-            responseBody.put("errors", Arrays.asList(exception.getMessage()));
-        }
-
-        responseBody.put("code", HttpStatus.NOT_FOUND.value());
-        responseBody.put("message", exception.getMessage());
-        return handleExceptionInternal(ex, responseBody, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    public ErrorMessage handleNotFoundException(RuntimeException ex, WebRequest request) {
+        return new ErrorMessage(
+                HttpStatus.NOT_FOUND.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
     }
 
     @Override

a9bff7a8ae4fabf4b095b4d506c253c858dec07a
cmsjulio <cms.julio1@gmail.com>
2023-09-03 23:07:45 -0300

    feat(PhotographerMapper): :sparkles: implementa mapper photographerToRegisterResponse

--- src/main/java/com/fotografocomvc/core/mapper/PhotographerMapper.java
+++ src/main/java/com/fotografocomvc/core/mapper/PhotographerMapper.java
@@ -2,13 +2,17 @@ package com.fotografocomvc.core.mapper;
 
 import com.fotografocomvc.api.http.resources.request.PhotographerRequest;
 import com.fotografocomvc.api.http.resources.response.PhotographerResponse;
+import com.fotografocomvc.api.http.resources.response.RegisterPhotographerResponse;
 import com.fotografocomvc.domain.model.Photographer;
+import com.fotografocomvc.domain.model.Role;
 import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
 
 @Component
 public class PhotographerMapper {
     public PhotographerResponse photographerToResponse (Photographer photographer){
-        return PhotographerResponse.builder()
+        PhotographerResponse photographerResponse = PhotographerResponse.builder()
                 .id(photographer.getId())
                 .name(photographer.getName())
                 .gender(photographer.getGender())
@@ -16,6 +20,10 @@ public class PhotographerMapper {
                 .phone(photographer.getPhone())
                 .aboutMe(photographer.getAboutMe())
                 .build();
+
+        photographerResponse.setRoles(photographer.getBaseUser().getRoles());
+
+        return photographerResponse;
     }
 
     public Photographer requestToPhotographer(PhotographerRequest photographerRequest) {
@@ -25,6 +33,18 @@ public class PhotographerMapper {
                 .bio(photographerRequest.getBio())
                 .phone(photographerRequest.getPhone())
                 .aboutMe(photographerRequest.getAboutMe())
+                .build();
+    }
+
+    public RegisterPhotographerResponse photographerToRegisterResponse(Photographer photographer) {
+        return RegisterPhotographerResponse.builder()
+                .email(photographer.getBaseUser().getUsername())
+                .roles(photographer.getBaseUser().getRoles().stream().map(Role::getName).collect(Collectors.toList()))
+                .name(photographer.getName())
+                .aboutMe(photographer.getAboutMe())
+                .phone(photographer.getPhone())
+                .gender(photographer.getGender().toString())
+                .bio(photographer.getBio())
                 .build();
     }
 }

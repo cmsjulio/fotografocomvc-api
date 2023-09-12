bd4506c5f29d2ce547ec790830001339c9522f07
cmsjulio <cms.julio1@gmail.com>
2023-09-12 00:05:27 -0300

    feat(BaseUserService): :sparkles: adicina método findByUsername

--- src/main/java/com/fotografocomvc/domain/service/BaseUserService.java
+++ src/main/java/com/fotografocomvc/domain/service/BaseUserService.java
@@ -2,11 +2,14 @@ package com.fotografocomvc.domain.service;
 
 import com.fotografocomvc.domain.model.BaseUser;
 
+import java.util.Optional;
+
 public interface BaseUserService {
 
     BaseUser save(BaseUser baseUser);
     BaseUser findById(Long id);
     BaseUser update(BaseUser baseUser);
     boolean existsByUsername(String username);
+    Optional<BaseUser> findByUsername(String email);
 
 }

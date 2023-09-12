0621d4c782a0c57524471533ff6ca82116a8e663
cmsjulio <cms.julio1@gmail.com>
2023-09-12 00:01:46 -0300

    refactor(SecurityConstants): :hammer: :construction: renomeia JWT_EXPIRATION para ACCESS_TOKEN_EXPIRATION e altera valores das constantes

--- src/main/java/com/fotografocomvc/api/security/SecurityConstants.java
+++ src/main/java/com/fotografocomvc/api/security/SecurityConstants.java
@@ -1,8 +1,8 @@
 package com.fotografocomvc.api.security;
 
 public class SecurityConstants {
-    public static final Long JWT_EXPIRATION = 1000L * 60 * 10;
+    // TODO ajustar tempos e alterar secret
+    public static final Long ACCESS_TOKEN_EXPIRATION = 1000L * 60 * 5;
     public static final String JWT_SECRET = "secret";
-
-    public static final Long REFRESH_TOKEN_EXPIRATION = 1000L * 10;
+    public static final Long REFRESH_TOKEN_EXPIRATION = 1000L * 60 * 10;
 }

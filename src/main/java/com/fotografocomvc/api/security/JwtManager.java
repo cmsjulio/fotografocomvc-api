fa3cba5335d108216d32225a7d1901c0e4264ded
cmsjulio <cms.julio1@gmail.com>
2023-09-11 23:56:54 -0300

    refactor(JwtManager): :hammer: jwtGenerator para jwtManager

--- src/main/java/com/fotografocomvc/api/security/JwtManager.java
+++ src/main/java/com/fotografocomvc/api/security/JwtManager.java
@@ -5,17 +5,32 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Component;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
+import java.security.Key;
+import java.time.Instant;
 import java.util.Date;
+import java.util.HashMap;
 
-@Component
-public class JwtGenerator {
+import static com.fotografocomvc.api.security.SecurityConstants.ACCESS_TOKEN_EXPIRATION;
+import static com.fotografocomvc.api.security.SecurityConstants.REFRESH_TOKEN_EXPIRATION;
 
-    public String generateToken (Authentication authentication){
+@Service
+public class JwtManager {
+
+    public String generateAccessJWT(Authentication authentication){
+        return generateToken(authentication, ACCESS_TOKEN_EXPIRATION);
+    }
+
+    public String generateRefreshJWT(Authentication authentication){
+        return generateToken(authentication, REFRESH_TOKEN_EXPIRATION);
+    }
+
+    public String generateToken (Authentication authentication, Long expirationTime){
         String username = authentication.getName();
         Date currentDate = new Date();
-        Date expiredDate = new Date(currentDate.getTime() + SecurityConstants.JWT_EXPIRATION);
+        Date expiredDate = new Date(currentDate.getTime() + expirationTime);
 
         return Jwts.builder()
                 .setSubject(username)
@@ -25,9 +40,9 @@ public class JwtGenerator {
                 .compact();
     }
 
-    public String generateTokenByEmail (String email){
+    public String generateTokenByEmail (String email, Long expirationTime){
         Date currentDate = new Date();
-        Date expiredDate = new Date(currentDate.getTime() + SecurityConstants.JWT_EXPIRATION);
+        Date expiredDate = new Date(currentDate.getTime() + expirationTime);
 
         return Jwts.builder()
                 .setSubject(email)

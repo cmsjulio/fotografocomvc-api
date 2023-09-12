95ba3351cf14fb60013d65ce71a942cf00436263
cmsjulio <cms.julio1@gmail.com>
2023-09-12 00:07:38 -0300

    feat(AcessToken): :sparkles: cria AccessToken

---
+++ src/main/java/com/fotografocomvc/domain/model/AccessToken.java
@@ -0,0 +1,35 @@
+package com.fotografocomvc.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "T_ACCESS_TOKEN")
+public class AccessToken {
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  @Column(name = "ID_ACCESS_TOKEN")
+  private long id;
+
+  @OneToOne(mappedBy = "accessToken")
+  private BaseUser baseUser;
+
+  @Column(nullable = false, unique = true)
+  private String tokenString;
+
+  public boolean isRevoked = false;
+
+  public boolean isExpired = false;
+
+
+}

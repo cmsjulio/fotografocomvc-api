c420b25b2843f436c728fbc1ee2caf064b9ef883
cmsjulio <cms.julio1@gmail.com>
2023-09-13 19:33:49 -0300

    refactor(PhotographerController): :hammer: :construction: refatora endpoint para teste de chamada autenticada com GET

--- src/main/java/com/fotografocomvc/api/controller/PhotographerController.java
+++ src/main/java/com/fotografocomvc/api/controller/PhotographerController.java
@@ -7,6 +7,7 @@ import com.fotografocomvc.core.mapper.PhotographerMapper;
 import com.fotografocomvc.domain.model.BaseUser;
 import com.fotografocomvc.domain.model.Photographer;
 import com.fotografocomvc.domain.repository.BaseUserRepository;
+import com.fotografocomvc.domain.service.BaseUserService;
 import com.fotografocomvc.domain.service.PhotographerService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
@@ -18,57 +19,25 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value = "/photographer")
 public class PhotographerController {
-    private final PhotographerService photographerService;
-    private final PhotographerMapper photographerMapper;
+    private final BaseUserService baseUserService;
 
-    private final BaseUserRepository baseUserRepository;
-
-    private final JwtManager jwtManager;
-
-    public PhotographerController(PhotographerService photographerService, PhotographerMapper photographerMapper, BaseUserRepository baseUserRepository, JwtManager jwtManager) {
-        this.photographerService = photographerService;
-        this.photographerMapper = photographerMapper;
-        this.baseUserRepository = baseUserRepository;
-        this.jwtManager = jwtManager;
+    public PhotographerController(BaseUserService baseUserService) {
+        this.baseUserService = baseUserService;
     }
 
     @Operation(tags = {"Photographer"}, description = "Create photographer")
-    @PostMapping
-    @ResponseBody
-    public ResponseEntity<PhotographerResponse> createPhotographer(@RequestBody @Valid PhotographerRequest photographerRequest, Authentication authentication) {
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        System.out.println("User has authorities: " + userDetails.getAuthorities());
-
-        BaseUser baseUser = baseUserRepository.findByUsername(userDetails.getUsername()).get();
-        Photographer photographer = photographerMapper.requestToPhotographer(photographerRequest);
-        photographer.setBaseUser(baseUser);
-        Photographer createdPhotographer = photographerService.save(photographer);
-
-        PhotographerResponse photographerResponse = photographerMapper.photographerToResponse(createdPhotographer);
-
-        return ResponseEntity.ok(photographerResponse);
-
-    }
-
-
-    @Operation(tags = {"Photographer"}, description = "Get photographer")
     @GetMapping
     @ResponseBody
-    public ResponseEntity<PhotographerResponse> getPhotographer(@RequestBody @Valid PhotographerRequest photographerRequest, Authentication authentication) {
+    public ResponseEntity<String> greetUser(Authentication authentication) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         System.out.println("User has authorities: " + userDetails.getAuthorities());
 
-        BaseUser baseUser = baseUserRepository.findByUsername(userDetails.getUsername()).get();
-        Photographer photographer = photographerMapper.requestToPhotographer(photographerRequest);
-        photographer.setBaseUser(baseUser);
-        Photographer createdPhotographer = photographerService.save(photographer);
+        BaseUser baseUser = baseUserService.findByUsername(userDetails.getUsername()).get();
 
-        PhotographerResponse photographerResponse = photographerMapper.photographerToResponse(createdPhotographer);
 
-        return ResponseEntity.ok(photographerResponse);
+        return ResponseEntity.ok("Bem vindo, " + baseUser.getUsername());
 
     }
-
 
     // updateByPhotographer - get the logged in photographer and update their profile;
 }

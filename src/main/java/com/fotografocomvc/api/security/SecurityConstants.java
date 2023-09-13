b5bfdfa4e90d7b4aa77649aa89f551ec3360dc03
cmsjulio <cms.julio1@gmail.com>
2023-09-13 19:35:13 -0300

    chore(SecurityConstants): :hammer: :sparkles: passa a chamar constantes do application.properties

--- src/main/java/com/fotografocomvc/api/security/SecurityConstants.java
+++ src/main/java/com/fotografocomvc/api/security/SecurityConstants.java
@@ -1,8 +1,13 @@
 package com.fotografocomvc.api.security;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SecurityConstants {
-    // TODO ajustar tempos e alterar secret
-    public static final Long ACCESS_TOKEN_EXPIRATION = 1000L * 60 * 5;
-    public static final String JWT_SECRET = "secret";
-    public static final Long REFRESH_TOKEN_EXPIRATION = 1000L * 60 * 10;
+    @Value("${fotografocomvc.app.jwtSecret}")
+    public static String JWT_SECRET;
+
+    @Value("${fotografocomvc.app.jwtExpirationMs}")
+    public static Long ACCESS_TOKEN_EXPIRATION;
+    @Value("${fotografocomvc.app.jwtRefreshExpirationMs}")
+    public static Long REFRESH_TOKEN_EXPIRATION;
 }

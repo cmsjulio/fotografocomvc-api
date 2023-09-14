6189e4e09fbda09cdc14536a4c3287f68a1a2751
cmsjulio <cms.julio1@gmail.com>
2023-09-14 00:21:48 -0300

    refactor(AccessTokenRepository): :hammer: :sparkles: remove méotod deleteAllByBaseUserId e implmenta findAllByBaseUserId

--- src/main/java/com/fotografocomvc/domain/repository/AccessTokenRepository.java
+++ src/main/java/com/fotografocomvc/domain/repository/AccessTokenRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -21,6 +22,6 @@ public interface AccessTokenRepository extends JpaRepository<AccessToken, Long> 
   Optional<AccessToken> findByBaseUserId(Long baseUserId);
 
   // TODO implementar no serviço para limpeza dos tokens? fazer o mesmo o refreshToken
-  void deleteAllByBaseUserId(Long baseUserId);
+  List<AccessToken> findAllByBaseUserId(Long baseUserId);
 
 }

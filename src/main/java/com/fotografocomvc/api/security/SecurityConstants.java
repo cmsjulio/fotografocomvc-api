9325497eb4dff984d852c2db2c2922e143dc33ff
cmsjulio <cms.julio1@gmail.com>
2023-09-30 08:24:03 -0300

    refactor(SecurityConstants): :hammer: remove utilização de @Value

--- src/main/java/com/fotografocomvc/api/security/SecurityConstants.java
+++ src/main/java/com/fotografocomvc/api/security/SecurityConstants.java
@@ -3,12 +3,7 @@ package com.fotografocomvc.api.security;
 import org.springframework.beans.factory.annotation.Value;
 
 public class SecurityConstants {
-    // TODO implementar configuração de constantes por application-properties via anotação @Value
-    @Value("${fotografocomvc.app.jwtSecret}")
     public static String JWT_SECRET = "eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY5NDY0MzE4MywiaWF0IjoxNjk0NjQzMTgzfQ";
-
-    @Value("${fotografocomvc.app.jwtExpirationMs}")
     public static Long ACCESS_TOKEN_EXPIRATION = 300000L;
-    @Value("${fotografocomvc.app.jwtRefreshExpirationMs}")
     public static Long REFRESH_TOKEN_EXPIRATION = 600000L;
 }

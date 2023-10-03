f0742406b5f42cc103ca190f4e237738ef22273f
cmsjulio <cms.julio1@gmail.com>
2023-10-03 06:49:42 -0300

    configura cors e corrige bug do Gender.toString()

--- src/main/java/com/fotografocomvc/api/controller/AuthController.java
+++ src/main/java/com/fotografocomvc/api/controller/AuthController.java
@@ -23,10 +23,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.stream.Collectors;
@@ -73,6 +70,7 @@ public class AuthController {
     }
 
     @PostMapping("/login")
+//    @CrossOrigin(origins = "http://localhost:4200")
     private ResponseEntity<AuthResponse> login (@RequestBody LoginRequest loginRequest){
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
                 loginRequest.getUsername(),loginRequest.getPassword()));
@@ -112,6 +110,7 @@ public class AuthController {
     }
 
     @PostMapping("/register-customer")
+//    @CrossOrigin(origins = "http://localhost:4200")
     private ResponseEntity<RegisterCustomerResponse> registerCustomer (@RequestBody RegisterCustomerRequest registerCustomerRequest){
         if (baseUserService.existsByUsername(registerCustomerRequest.getUsername())){
             throw new BusinessException("Username is taken");
@@ -139,6 +138,7 @@ public class AuthController {
     }
 
     @PostMapping("/register-photographer")
+//    @CrossOrigin(origins = "http://localhost:4200")
     private ResponseEntity<RegisterPhotographerResponse> registerPhotographer (@Valid @RequestBody RegisterPhotographerRequest registerPhotographerRequest){
         if (baseUserService.existsByUsername(registerPhotographerRequest.getUsername())){
             throw new BusinessException("Username is taken");
@@ -170,6 +170,7 @@ public class AuthController {
     }
 
     @PostMapping("/refreshtoken")
+//    @CrossOrigin(origins = "http://localhost:4200")
     public ResponseEntity<?> refreshtoken(@Valid @RequestBody TokenRefreshRequest request) {
         // TODO checar validação, apagar token atual e gerar novo
         String requestRefreshToken = request.getRefreshToken();

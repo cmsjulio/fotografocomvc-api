a7ce7643ff614987718ca29e802276191e82f316
cmsjulio <cms.julio1@gmail.com>
2023-10-05 16:05:15 -0300

    fix: corrige erro no handler de BusinessException

--- src/main/java/com/fotografocomvc/domain/exception/BusinessException.java
+++ src/main/java/com/fotografocomvc/domain/exception/BusinessException.java
@@ -3,7 +3,6 @@ package com.fotografocomvc.domain.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.Map;
 @ResponseStatus(HttpStatus.CONFLICT)
 public class BusinessException extends RuntimeException {
     private static final long serialVersionUID = -4047125885447267412L;
